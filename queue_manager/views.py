b7d2794a2972027812f476e83c8d81791edf6d02
Pattapon Gowanit <iampattapon@gmail.com>
2024-10-09 21:23:04 +0700

    Add index view

--- queue_manager/views.py
+++ queue_manager/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class MainView(generic.TemplateView):
+    template_name = 'queue_manager/index.html'
