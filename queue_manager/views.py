cda337516807909952b2b9436ca4b0df40928331
Pattapon Gowanit <iampattapon@gmail.com>
2024-10-11 20:40:53 +0700

    Add get_queryset method to IndexView

--- queue_manager/views.py
+++ queue_manager/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
+from queue_manager.models import *
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
-class MainView(generic.TemplateView):
+class IndexView(generic.ListView):
     template_name = 'queue_manager/index.html'
+    context_object_name = 'queue_list'
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return Queue.objects.filter(participant__user=self.request.user)
+        else:
+            return Queue.objects.none()
+
+
