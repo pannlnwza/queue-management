1dd5b6f11fffc18e6fafd36a1dcb75c49d721b85
Pattapon Gowanit <iampattapon@gmail.com>
2024-10-13 18:34:35 +0700

    Show messages when joining queue

--- queue_manager/urls.py
+++ queue_manager/urls.py
@@ -5,5 +5,6 @@ app_name = 'queue'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('create', CreateQView.as_view(), name='create_q'),
-path('queues/', QueueListView.as_view(), name='queues'),
+    path('queues/', QueueListView.as_view(), name='queues'),
+    path('join/', join_queue, name='join'),
 ]
