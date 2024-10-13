3da8195ef1ce85a989578a2a7b39607f1310a139
tanasatit <tanasatit.n@ku.th>
2024-10-13 21:20:49 +0700

    write docstring for all TestCase

--- queue_manager/tests/test_join_queue.py
+++ queue_manager/tests/test_join_queue.py
@@ -2,17 +2,25 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from queue_manager.models import Queue, Participant
-from django.contrib import messages
 
 
 class JoinQueueViewTests(TestCase):
+    """
+    Tests for the JoinQueue view functionality.
+    """
 
     def setUp(self):
+        """
+        Set up the test case by creating a test user and a test queue.
+        """
         self.user = User.objects.create_user(username='testuser', password='testpassword')
         self.queue = Queue.objects.create(name='Test Queue', description='A test queue')
 
     def test_join_queue_success(self):
-        self.client.login(username='testuser', password='testpassword')  # Log the user in
+        """
+        Test that a user can successfully join a queue.
+        """
+        self.client.login(username='testuser', password='testpassword')
         response = self.client.post(reverse('queue:join_queue'), {'queue_code': self.queue.code})
 
         participant_exists = Participant.objects.filter(user=self.user, queue=self.queue).exists()
@@ -20,10 +28,12 @@ class JoinQueueViewTests(TestCase):
 
         messages_list = list(response.wsgi_request._messages)
         self.assertEqual(str(messages_list[0]), "You have successfully joined the queue.")
-
         self.assertRedirects(response, reverse('queue:index'))
 
     def test_join_queue_already_in_queue(self):
+        """
+        Test that a user who is already in a queue receives the correct message.
+        """
         self.client.login(username='testuser', password='testpassword')
         self.client.post(reverse('queue:join_queue'), {'queue_code': self.queue.code})
 
@@ -34,6 +44,9 @@ class JoinQueueViewTests(TestCase):
         self.assertRedirects(response, reverse('queue:index'))
 
     def test_join_queue_invalid_code(self):
+        """
+        Test that a user receives an error message when trying to join a queue with an invalid code.
+        """
         self.client.login(username='testuser', password='testpassword')
         response = self.client.post(reverse('queue:join_queue'), {'queue_code': 'INVALIDCODE'})
 
@@ -42,5 +55,4 @@ class JoinQueueViewTests(TestCase):
 
         messages_list = list(response.wsgi_request._messages)
         self.assertEqual(str(messages_list[0]), "Invalid queue code.")
-
         self.assertRedirects(response, reverse('queue:index'))
--- queue_manager/tests/test_signup.py
+++ queue_manager/tests/test_signup.py
@@ -4,18 +4,26 @@ from django.contrib.auth.models import User
 
 
 class SignupTest(TestCase):
+    """
+    Tests for the signup functionality.
+    """
+
     def test_signup_page_status_code(self):
-        """Test that signup page returns a 200 status code."""
+        """
+        Test that the signup page returns a 200 status code.
+        """
         response = self.client.get(reverse('signup'))
         self.assertEqual(response.status_code, 200)
 
     def test_signup_valid_data(self):
-        """Test that a valid form submission creates a new user."""
+        """
+        Test that a valid form submission creates a new user.
+        """
         response = self.client.post(reverse('signup'), {
             'username': 'testuser',
             'password1': 'testpassword123',
             'password2': 'testpassword123'
         })
-        self.assertEqual(response.status_code, 302)  # Should redirect after successful signup
+        self.assertEqual(response.status_code, 302)
         user = User.objects.filter(username='testuser').first()
-        self.assertIsNotNone(user)  # Check that the user was created
+        self.assertIsNotNone(user)
