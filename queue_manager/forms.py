801fa9d0eb8b64f7442bca3c52f7e75e5145ddfc
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-10-22 17:12:44 +0700

    Add capacity in create queue form.

--- queue_manager/forms.py
+++ queue_manager/forms.py
@@ -17,7 +17,16 @@ class QueueForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Queue Name'}),
             'description': forms.Textarea(
                 attrs={'class': 'form-control', 'placeholder': 'Enter Description', 'rows': 4}),
+            'capacity': forms.NumberInput(attrs={'class': 'form-control',
+                                                 'placeholder': 'Enter Queue Capacity'}),
             'estimated_wait_time': forms.NumberInput(attrs={'class': 'form-control',
                                                             'placeholder': 'Enter Estimated Wait Time (minutes)'}),
         }
 
+    name = forms.CharField(required=True, error_messages={
+        'required': 'Please enter a name for the queue.'})
+    estimated_wait_time = forms.IntegerField(required=True, error_messages={
+        'required': 'Please enter an estimated wait time.'})
+    capacity = forms.IntegerField(required=True, error_messages={
+        'required': 'Please enter a capacity for the queue.'})
+    description = forms.CharField(required=False)
