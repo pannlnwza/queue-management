ebf960dfe75aabfe8ce587ef1d92dbc94f671da4
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-10-23 23:27:59 +0700

    Add url of each queue type

--- queue_manager/urls.py
+++ queue_manager/urls.py
@@ -5,11 +5,14 @@ app_name = 'queue'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('create', CreateQView.as_view(), name='create_q'),
-    path('queues/', QueueListView.as_view(), name='queues'),
+    path('queues/', BrowseQueueView.as_view(), name='queues'),
     path('join/', join_queue, name='join'),
     path('dashboard/<int:pk>/', QueueDashboardView.as_view(), name='dashboard'),
     path('delete/<int:participant_id>/', delete_participant, name='delete'),
     path('manage/', ManageQueuesView.as_view(), name='manage_queues'),
     path('queue/<int:pk>/edit/', EditQueueView.as_view(), name='edit_queue'),
+    path('queues/restaurant/', RestaurantQueueView.as_view(), name='restaurant_queues'),
+    path('queues/general/', GeneralQueueView.as_view(), name='general_queues'),
+    path('queues/hospital/', HospitalQueueView.as_view(), name='hospital_queues'),
 ]
 
