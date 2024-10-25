eb2c370c695e2bd720e216f94c3cac820b13ad4e
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-10-25 14:04:06 +0700

    Add method to get queue logo in Queue model in models.py

--- queue_manager/models.py
+++ queue_manager/models.py
@@ -3,6 +3,7 @@ import random
 from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
+from django.templatetags.static import static
 
 
 class Queue(models.Model):
@@ -24,10 +25,12 @@ class Queue(models.Model):
     description = models.TextField()
     code = models.CharField(max_length=6, unique=True, editable=False)
     estimated_wait_time = models.PositiveIntegerField(default=0)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
+                                   blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='open')
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES,
+                              default='open')
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
     logo = models.ImageField(upload_to='queue_logos/', blank=True, null=True)
 
@@ -40,14 +43,16 @@ class Queue(models.Model):
             self.code = self.generate_unique_code()
         super().save(*args, **kwargs)
 
-    def update_estimated_wait_time(self, average_time_per_participant: int) -> None:
+    def update_estimated_wait_time(self,
+                                   average_time_per_participant: int) -> None:
         """Update the estimated wait time based on the number of participants.
 
         :param average_time_per_participant: Average time per participant in minutes.
         :raises ValueError: If the average time per participant is negative.
         """
         if average_time_per_participant < 0:
-            raise ValueError("Average time per participant cannot be negative.")
+            raise ValueError(
+                "Average time per participant cannot be negative.")
 
         num_participants = self.participant_set.count()
         self.estimated_wait_time = num_participants * average_time_per_participant
@@ -82,8 +87,20 @@ class Queue(models.Model):
         today = timezone.now().date()
         return self.participant_set.filter(created_at__date=today).count()
 
+    def get_logo_url(self):
+        """Get a logo url for queue."""
+        if self.logo:
+            return self.logo.url
+        default_logos = {
+            'restaurant': static(
+                'queue_manager/images/restaurant_default_logo.jpg'),
+            'bank': static('queue_manager/images/bank_default_logo.jpg'),
 
-    def edit(self, name: str = None, description: str = None, is_closed: bool = None, status: str = None) -> None:
+        }
+        return default_logos.get(str(self.category))
+
+    def edit(self, name: str = None, description: str = None,
+             is_closed: bool = None, status: str = None) -> None:
         """
         Edit the queue's name, description, or closed status.
 
@@ -95,7 +112,8 @@ class Queue(models.Model):
         """
         if name is not None:
             if len(name) < 1 or len(name) > 255:
-                raise ValueError("The name must be between 1 and 255 characters.")
+                raise ValueError(
+                    "The name must be between 1 and 255 characters.")
             self.name = name
         if description is not None:
             self.description = description
@@ -132,7 +150,9 @@ class Queue(models.Model):
 class UserProfile(models.Model):
     """Represents a user profile in the system."""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_type = models.CharField(max_length=20, choices=[('creator', 'Queue Creator'), ('participant', 'Participant')])
+    user_type = models.CharField(max_length=20,
+                                 choices=[('creator', 'Queue Creator'),
+                                          ('participant', 'Participant')])
     phone_no = models.CharField(max_length=15)
 
     def __str__(self) -> str:
