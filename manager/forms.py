36d2a4b29abab22a7c0d5b2ec4889af71a82e475
Pannatat Nakornpuckdee <pannatat.na@ku.th>
2024-11-11 13:50:30 +0700

    Modify forms.py to has latitude and longitude.

--- manager/forms.py
+++ manager/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Queue  # Assuming Queue model is in the same app
+from .models import Queue
+
 
 class QueueForm(forms.ModelForm):
     """
@@ -8,15 +9,26 @@ class QueueForm(forms.ModelForm):
     This form allows users to input the necessary details for a queue,
     including its name and description.
     """
+
     class Meta:
         model = Queue
-        fields = ['name', 'logo', 'description', 'category']
+        fields = ['name', 'logo', 'description', 'category', 'latitude',
+                  'longitude']
         labels = {
             'logo': 'Logo (Optional)',
         }
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'input input-bordered w-full max-w-xs m-2', 'placeholder': 'Enter Queue Name (Max Length: 50)'}),
-            'description': forms.Textarea(attrs={'class': 'textarea textarea-bordered w-full m-2', 'placeholder': 'Enter Description (Max Length: 100)', 'rows': 4}),
-            'category': forms.Select(attrs={'class': 'select select-bordered w-full max-w-xs m-2'}),
-            'logo': forms.ClearableFileInput(attrs={'class': 'file-input w-full max-w-xs m-2', 'accept': 'image/*'}),
+            'name': forms.TextInput(
+                attrs={'class': 'input input-bordered w-full max-w-xs m-2',
+                       'placeholder': 'Enter Queue Name (Max Length: 50)'}),
+            'description': forms.Textarea(
+                attrs={'class': 'textarea textarea-bordered w-full m-2',
+                       'placeholder': 'Enter Description (Max Length: 100)',
+                       'rows': 4}),
+            'category': forms.Select(
+                attrs={'class': 'select select-bordered w-full max-w-xs m-2'}),
+            'logo': forms.ClearableFileInput(
+                attrs={'class': 'file-input w-full max-w-xs m-2',
+                       'accept': 'image/*'}),
+
         }
