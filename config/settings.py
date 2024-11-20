08962f6910cff3d9bf89668169d9518cb4aa8611
Thanawat Tantijaroensin <taroofsatoeimo@gmail.com>
2024-11-21 03:41:58 +0700

    chore: Add HOST_USER_EMAIL and password to settings.py

--- config/settings.py
+++ config/settings.py
@@ -130,10 +130,16 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTHENTICATION_BACKENDS = [
     # username & password authentication
     'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend' # for google
+    'allauth.account.auth_backends.AuthenticationBackend'
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
