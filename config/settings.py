5577307e60c063ee8dbe975cc04d57b071411cb8
Thanawat Tantijaroensin <taroofsatoeimo@gmail.com>
2024-11-22 11:14:31 +0700

    feat: change settings.py to use postgresql but it doesn't work.

--- config/settings.py
+++ config/settings.py
@@ -103,8 +103,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "Queue_management_db",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
