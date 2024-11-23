bef99c606b59cd2402cbffc3a5b1340963f5abbd
Pattapon Gowanit <iampattapon@gmail.com>
2024-11-23 22:55:02 +0700

    chore: Change staticfiles dir

--- config/settings.py
+++ config/settings.py
@@ -185,12 +185,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_URL = 'static/'
-STATICFILES_URL = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
