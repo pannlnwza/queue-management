58e14b47c0055ac9dc95c559b22a965de860931f
Pattapon Gowanit <iampattapon@gmail.com>
2024-11-23 23:07:35 +0700

    chore: Add staticfile storage

--- config/settings.py
+++ config/settings.py
@@ -190,6 +190,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
