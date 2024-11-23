0a1187f828c372a8de1d33b4340caed966822ba6
Pattapon Gowanit <iampattapon@gmail.com>
2024-11-24 04:52:31 +0700

    chore: Modify database settings

--- config/settings.py
+++ config/settings.py
@@ -106,6 +106,10 @@ DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
+DATABASES['default'] = dj_database_url.config(
+    default=config('DATABASE_URL'),
+    conn_max_age=300,
+    ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
