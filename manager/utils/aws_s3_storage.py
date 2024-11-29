ee9f51574fcf4a0ef81bcff3b69a9827dd254107
tanasatit <tanasatit.n@ku.th>
2024-11-29 13:27:40 +0700

    feat: create file name aws_s3_storage.py for upload and get file

---
+++ manager/utils/aws_s3_storage.py
@@ -0,0 +1,44 @@
+import boto3
+import os
+from django.conf import settings
+import uuid
+
+
+def upload_to_s3(file, folder):
+    """
+    Upload a file to S3 in a specified folder.
+
+    Args:
+        file: The file object to upload.
+        folder: The folder in the S3 bucket where the file should be stored.
+
+    Returns:
+        str: The public URL of the uploaded file.
+    """
+    print(f"this is file name: {file.name}")
+    file_extension = os.path.splitext(file.name)[-1]
+    unique_file_name = f"{uuid.uuid4().hex}{file_extension}"
+    file_key = f"{folder}/{unique_file_name}"
+    print(f"this is unique file name: {unique_file_name}")
+
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+        region_name=settings.AWS_STORAGE_REGION
+    )
+    bucket_name = settings.AWS_STORAGE_BUCKET
+
+    try:
+        s3_client.upload_fileobj(file, bucket_name, file_key, ExtraArgs={'ACL': 'public-read'})
+        file_url = f"https://{bucket_name}.s3.{settings.AWS_STORAGE_REGION}.amazonaws.com/{file_key}"
+        return file_url
+    except Exception as e:
+        raise Exception(f"Failed to upload file to STORAGE: {e}")
+
+
+def get_s3_base_url():
+    """
+    Returns the base STORAGE URL for the configured bucket and region.
+    """
+    return f"https://{settings.AWS_STORAGE_BUCKET}.s3.{settings.AWS_STORAGE_REGION}.amazonaws.com/"
