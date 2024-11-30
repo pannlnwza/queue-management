8b0e25ba6f26bd49f2dc83e9504474f35ad71337
Pattapon Gowanit <iampattapon@gmail.com>
2024-11-30 20:34:03 +0700

    feat: Make the kiosk page more user friendly

--- participant/forms.py
+++ participant/forms.py
@@ -4,7 +4,6 @@ from participant.models import RestaurantParticipant, HospitalParticipant, BankP
 
 
 class KioskForm(forms.Form):
-    # Common fields for all categories
     name = forms.CharField(
         widget=forms.TextInput(attrs={'class': 'input input-bordered w-full', 'placeholder': 'Enter Your Name'}),
         max_length=50
@@ -22,7 +21,6 @@ class KioskForm(forms.Form):
     def __init__(self, *args, queue=None, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Add specific fields for each queue category
         if queue:
             if queue.category == 'restaurant':
                 self.fields['special_1'] = forms.IntegerField(
@@ -32,7 +30,7 @@ class KioskForm(forms.Form):
                 )
                 self.fields['special_2'] = forms.ChoiceField(
                     choices=RestaurantParticipant.SERVICE_TYPE_CHOICE,
-                    widget=forms.Select(attrs={'class': 'select select-bordered w-full select-md'}),
+                    widget=forms.Select(attrs={'class': 'select select-bordered w-full'}),
                     required=False,
                     initial='first_available',
                     label='Seating Preference',
@@ -40,31 +38,31 @@ class KioskForm(forms.Form):
             elif queue.category == 'hospital':
                 self.fields['special_1'] = forms.ChoiceField(
                     choices=HospitalParticipant.MEDICAL_FIELD_CHOICES,
-                    widget=forms.Select(attrs={'class': 'select select-bordered w-full select-md max-w-xs'}),
+                    widget=forms.Select(attrs={'class': 'select select-bordered w-full'}),
                     required=True,
                     label="Medical Field",
                 )
                 self.fields['special_2'] = forms.ChoiceField(
                     choices=HospitalParticipant.PRIORITY_CHOICES,
-                    widget=forms.Select(attrs={'class': 'select select-bordered w-full select-md max-w-xs'}),
+                    widget=forms.Select(attrs={'class': 'select select-bordered w-full'}),
                     required=True,
                     label='Priority Level',
                 )
             elif queue.category == 'bank':
                 self.fields['special_1'] = forms.ChoiceField(
                     choices=BankParticipant.PARTICIPANT_CATEGORY_CHOICES,
-                    widget=forms.Select(attrs={'class': 'select select-bordered w-full select-md max-w-xs'}),
+                    widget=forms.Select(attrs={'class': 'select select-bordered w-full'}),
                     required=True,
                     label="Select Your Participant Type",
                 )
                 self.fields['special_2'] = forms.ChoiceField(
                     choices=BankParticipant.SERVICE_TYPE_CHOICES,
-                    widget=forms.Select(attrs={'class': 'select select-bordered w-full select-md max-w-xs'}),
+                    widget=forms.Select(attrs={'class': 'select select-bordered w-full max-w-xs'}),
                     required=False,
                     label='Select Service Type',
                 )
             self.fields['note'] = forms.CharField(
                 required=False,
                 widget=forms.TextInput(
-                    attrs={'class': 'input input-bordered w-full', 'placeholder': "Additional notes"})
+                    attrs={'class': 'textarea textarea-bordered w-full h-24', 'placeholder': "Additional notes"})
             )
